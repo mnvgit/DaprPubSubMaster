3c26f9f7554e4581a5c0d58086855b42bfa197b9
Michael N <michaelnv26@gmail.com>
2025-08-25 09:16:36 +0300

    Add DaprPublisherWorker

---
+++ DaprPublisherWorker/Program.cs
@@ -0,0 +1,8 @@
+﻿using DaprPublisherWorker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddDaprClient();
+builder.Services.AddHostedService<PublisherWorker>();
+await builder.Build().RunAsync();
---
+++ DaprPublisherWorker/PublisherWorker.cs
@@ -0,0 +1,32 @@
+﻿using Dapr.Client;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DaprPublisherWorker
+{
+    public record Order(int Id, DateTime Timestamp);
+
+    public class PublisherWorker : BackgroundService
+    {
+        private readonly DaprClient _dapr;
+        private readonly ILogger<PublisherWorker> _logger;
+
+        public PublisherWorker(ILogger<PublisherWorker> logger, DaprClient dapr)
+        {
+            _logger = logger;
+            _dapr = dapr;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken ct)
+        {
+            int counter = 0;
+            while (!ct.IsCancellationRequested)
+            {
+                var msg = new Order(counter++, DateTime.UtcNow);
+                await _dapr.PublishEventAsync("pubsub", "orders", msg, ct);
+                _logger.LogInformation("Published: {msg}", msg);
+                await Task.Delay(5000, ct); // every 5s
+            }
+        }
+    }
+}
